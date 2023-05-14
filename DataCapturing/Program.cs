2812361bca0431322f506f84e18da7f3acb29d93
Aydin Valiyev <aydin0294@gmail.com>
2023-05-14 17:32:51 +0200

    added message queues

---
+++ DataCapturing/Program.cs
@@ -0,0 +1,83 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace DataCapturing;
+
+public class Program
+{
+    private const string SourceFolder = @"C:\Users\Aydin\source\repos\MessageQueues\Files";
+    private const string ServiceBusConnectionString = "Endpoint=sb://aydinservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=gnSD9zAicdprmvrvRRo36H1U9tmHVISHI+ASbGmelBY=";
+    private const string TopicName = "files";
+
+    private const int chunkSize = 256000;
+
+    private static ServiceBusClient _serviceBusClient;
+    private static ServiceBusSender _serviceBusSender;
+
+
+    static void Main(string[] args)
+    {
+        MainMessageAsync().GetAwaiter().GetResult();
+    }
+
+    static async Task MainMessageAsync()
+    {
+        _serviceBusClient = new ServiceBusClient(ServiceBusConnectionString);
+        _serviceBusSender = _serviceBusClient.CreateSender(TopicName);
+
+        var path = Path.Combine(SourceFolder);
+        var fileWatcher = new FileSystemWatcher()
+        {
+            Path = path,
+            EnableRaisingEvents = true
+        };
+
+        fileWatcher.Created += FileWatcher_CreatedAsync;
+
+        Console.WriteLine("Press Enter to stop the service");
+
+        var input = Console.ReadKey();
+        if (input.Key == ConsoleKey.Enter)
+        {
+            await _serviceBusSender.CloseAsync();
+        }
+    }
+
+    private static async void FileWatcher_CreatedAsync(object sender, FileSystemEventArgs e)
+    {
+        try
+        {
+            int count = 0;
+                using (FileStream fileStream = File.OpenRead(e.FullPath))
+                {
+                    while (fileStream.Position != fileStream.Length)
+                    {
+                        var buffer = new byte[chunkSize];
+
+                        var message = new ServiceBusMessage(buffer)
+                        {
+                            Subject = e.Name
+                        };
+
+                        message.ApplicationProperties.Add("position", fileStream.Position);
+
+                        var readBytes = fileStream.Read(buffer, 0, chunkSize);
+
+                        message.ApplicationProperties.Add("size", readBytes);
+                        message.ApplicationProperties.Add("fileSize", fileStream.Length);
+
+                        await _serviceBusSender.SendMessageAsync(message);
+
+                        count += readBytes;
+                        Console.WriteLine("Sent: {0}", count);
+                    }
+                }           
+
+            count = 0;
+            Console.WriteLine($"File {e.Name} successfully sent");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ MainProcessing/Program.cs
@@ -0,0 +1,72 @@
+﻿using Azure.Messaging.ServiceBus;
+
+class Program
+{
+    private const string SourceFolder = @"C:\Users\Aydin\source\repos\MessageQueues\Downloads";
+
+    private const string ServiceBusConnectionString = "Endpoint=sb://aydinservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=gnSD9zAicdprmvrvRRo36H1U9tmHVISHI+ASbGmelBY=";
+    private const string TopicName = "files";
+    private const string SubscriptionName = "filesubscription";
+    static string currentFile = string.Empty;
+    static string currentFilePath = string.Empty;
+    static int bytesCount = 0;
+    private static ServiceBusClient _serviceBusClient;
+    private static ServiceBusProcessor _serviceBusProcessor;
+    static void Main(string[] args)
+    {
+        ReceiveMessageAsync().GetAwaiter().GetResult();
+    }
+
+    static async Task ReceiveMessageAsync()
+    {
+        _serviceBusClient = new ServiceBusClient(ServiceBusConnectionString);
+        _serviceBusProcessor = _serviceBusClient.CreateProcessor(TopicName, SubscriptionName);
+        _serviceBusProcessor.ProcessMessageAsync += ProcessMessageAsync;
+        _serviceBusProcessor.ProcessErrorAsync += ExceptionReceivedHandler;
+
+        await _serviceBusProcessor.StartProcessingAsync();
+
+        Console.ReadKey();
+
+        await _serviceBusProcessor.StopProcessingAsync();
+    }
+
+
+    static async Task ProcessMessageAsync(ProcessMessageEventArgs eventArgs)
+    {
+        string fileName = eventArgs.Message.Subject;
+
+        if (currentFile != fileName)
+        {
+            Console.WriteLine("Retrieving new file {0}", fileName);
+            currentFilePath = Path.Combine(SourceFolder, fileName);
+            currentFile = fileName;
+        }
+
+        var position = Convert.ToInt64(eventArgs.Message.ApplicationProperties["position"]);
+        var size = Convert.ToInt32(eventArgs.Message.ApplicationProperties["size"]);
+        var fileSize = Convert.ToInt64(eventArgs.Message.ApplicationProperties["fileSize"]);
+
+        using (var fileStream = new FileStream(currentFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+        {
+            fileStream.Position = position;
+            await fileStream.WriteAsync(eventArgs.Message.Body.ToArray(), 0, size);
+            await fileStream.FlushAsync();
+        }
+
+        bytesCount += size;
+        Console.WriteLine("Current size: {0}", bytesCount);
+
+        if (bytesCount == fileSize)
+        {
+            Console.WriteLine($"Received file {fileName} and saved it");
+            bytesCount = 0;
+        }
+    }
+
+    static Task ExceptionReceivedHandler(ProcessErrorEventArgs eventArgs)
+    {
+        Console.WriteLine($"Message handler encountered an exception {eventArgs.Exception}");
+        return Task.CompletedTask;
+    }
+}
